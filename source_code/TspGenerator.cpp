0893d5bf400c9165f2ff848f4041f70fe470e09d
karol22git <kamel.poczta676@gmail.com>
2024-03-28 20:09:55 +0100

    working on TspGenerator class test

--- source_code/TspGenerator.cpp
+++ source_code/TspGenerator.cpp
@@ -4,15 +4,21 @@
 #include <fstream>
 
 TspGenerator::TspGenerator() {
-    generator  = std::make_unique<std::mt19937>(125230);
+    generator  = std::make_unique<std::mt19937>(time(NULL));
 }
 
 std::vector<struct City> TspGenerator::GenerateSymmetricTsp(unsigned int how_many, unsigned int min, unsigned int max) {
     std::vector<struct City> city_list;
     for(int i = 0 ; i < how_many ; ++i) {
         struct City new_city;
-        new_city.x = ((*generator)() % max) + min;
-        new_city.y = ((*generator)() % max) + min;
+        new_city.x = ((*generator)() % max) ;
+        new_city.y = ((*generator)() % max) ;
+        if (new_city.x < min) {
+            new_city.x = new_city.x + min;
+        }
+        if(new_city.y < min) {
+            new_city.y = new_city.y + min;
+        }
         city_list.push_back(new_city);
     }
     return city_list;
@@ -20,11 +26,14 @@ std::vector<struct City> TspGenerator::GenerateSymmetricTsp(unsigned int how_man
 
 void TspGenerator::SaveDataset(std::string file_name,std::vector<City> city_list) {
     std::ofstream file(file_name);
+    file << "n = "<< city_list.size()<<"\n";
+    file << "x ; y"<<std::endl;
+    file <<"Data: "<<std::endl;
     for(auto it = begin(city_list) ; it != end(city_list) ; ++it) {
         file << it->x;
         file << ";";
         file << it->y;
-        file << ";\n"; 
+        file << std::endl; 
     }
     file.close();
     return;
--- tests/tspgenerator_test.cpp
+++ tests/tspgenerator_test.cpp
@@ -6,17 +6,25 @@
 #include "../include/City.hpp"
 #include <boost/test/unit_test.hpp>
 #include <ctime>
+#include <fstream>
+#include <iostream>
 
 BOOST_AUTO_TEST_SUITE( TspGeneratorClassTest )
 
 BOOST_AUTO_TEST_CASE(test1, *boost::unit_test::description("Testing generating data with TspGenerator class")) {
-    std::string exampleseed = "exampleseed";
-    unsigned seed2 = 10000000;
-    std::seed_seq seed1 (exampleseed.begin(),exampleseed.end());
     std::unique_ptr<TspGenerator> generator = std::make_unique<TspGenerator>();
-    //TspGenerator* generator = new TspGenerator();
-    std::vector<struct City> test_dataset = generator->GenerateSymmetricTsp(50,5,100);
-    generator->SaveDataset("../data/data_test.txt",test_dataset);
+
+    unsigned int min = 5;
+    unsigned int max = 100;
+    unsigned int n = 50;
+    
+    std::string file = "../data/data_test.txt";
+    std::vector<struct City> test_dataset = generator->GenerateSymmetricTsp(n,min,max);
+    generator->SaveDataset(file,test_dataset);
+    std::ifstream MyFile(file);
+
+
+
 }
 
 BOOST_AUTO_TEST_SUITE_END()
